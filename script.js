ecdba0bf145d034618faef79b41ee438e20d6b71
Abi Garner <abigail.gee@hotmail.com>
2024-04-15 01:13:08 -0400

    solved 2nd question

--- script.js
+++ script.js
@@ -29,11 +29,34 @@ let n=22;
 while (n < 30) {
     n++;
 
-    if(n % 2 || n % 3 || n % 5 !==0) {
-        
-    console.log(n); }
+    if(n % 2 && n % 5 !==0) {
+       if (n % 3 !==0)
+    console.log(n); 
+    continue; }
     
-    break;
+    // break;
     
 
 }
+
+
+////// testing with larger numbers
+
+// for(g = 85;g < 90;g++) {
+
+
+//     if(g % 2 || 3 !==0)
+        
+//        console.log(g);
+   
+// }
+// console.log(g);
+// if (g % 3 === 0) {
+//     console.log(g);
+// }
+   
+//     if (g % 5 === 0) {
+//         console.log(g);
+        
+//     }
+
