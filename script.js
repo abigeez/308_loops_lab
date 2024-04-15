6144fbdffdb1059a96c460d958a8b5297762b2a3
Abi Garner <abigail.gee@hotmail.com>
2024-04-14 22:49:35 -0400

    fizz buzz part 1 completed

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 
+//fizz buzz problem
+
 let fb1 = 1;
 let f1 = "fizz";
 let b1 = "buzz";
+let fb2 = "Fizz Buzz";
 while (fb1 < 100) {
 
  fb1++;   
@@ -13,5 +16,10 @@ while (fb1 < 100) {
   if(fb1 % 5 == 0) {
     console.log(b1);
 }
+ if( fb1 % 5 === 0 || 3 === 0) {
+ console.log(fb2) }
 
+if (fb1 % 3 || 5 !==0) {
+    console.log(fb1)
+}
 }
