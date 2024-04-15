0421ef4b4f766a7004ab9bbca135d96574ac7f77
Abi Garner <abigail.gee@hotmail.com>
2024-04-14 22:11:52 -0400

    First two parts of fizz buzz done

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let fb1 = 1;
+let f1 = "fizz";
+let b1 = "buzz";
+while (fb1 < 100) {
+
+ fb1++;   
+
+ if (fb1 % 3 == 0) {
+    console.log(f1); 
+ }
+
+  if(fb1 % 5 == 0) {
+    console.log(b1);
+}
+
+}
